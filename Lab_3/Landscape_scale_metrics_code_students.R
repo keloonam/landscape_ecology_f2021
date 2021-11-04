bd8deaa3917dd5e1b6088ae42fcf4328497d03cc
Kenneth Loonam <keloonam@gmail.com>
2021-11-04 14:24:53 -0700

    Going home to work. Answered lab 3 q 1

--- Lab_3/Landscape_scale_metrics_code_students.R
+++ Lab_3/Landscape_scale_metrics_code_students.R
@@ -6,7 +6,7 @@ library(raster)
 library (psych)
 list_lsm() #gives full list of metrics by patch, class and landscape (very useful)
 data.frame (list_lsm()) #to see full list (not a tibble)
-setwd("~/Documents/Landscape Ecology 2019/Landscape_Metrics") #change file path to where your data are stored
+# setwd("~/Documents/Landscape Ecology 2019/Landscape_Metrics") #change file path to where your data are stored
 
 #######Data set-up#######
 
@@ -16,7 +16,7 @@ setwd("~/Documents/Landscape Ecology 2019/Landscape_Metrics") #change file path 
 #If you want to look at the whole NLCD map: https://spatialdata.oregonexplorer.info/geoportal/download 
 
 #You can download the Oregon NLCD to your working directory and view in R using the following code:
-tile = raster("NLCD_OR_2011_.tif") 
+tile = raster("Data//lab_3//NLCD_OR_2011_.tif") 
 plot (tile)
 #Legend available here: https://www.mrlc.gov/data/legends/national-land-cover-database-2011-nlcd2011-legend
 
@@ -24,16 +24,16 @@ plot (tile)
 #Data import
 
 #Import rasters that have been saved to your working directory.
-land1 = raster("land1.tif")
-land2 = raster("land2.tif")
-land3 = raster("land3.tif")
-land4 = raster("land4.tif")
-land5 = raster("land5.tif")
-land6 = raster("land6.tif")
-land7 = raster("land7.tif")
-land8 = raster("land8.tif")
-land9 = raster("land9.tif")
-land10 = raster("land10.tif")
+land1 = raster("Data//lab_3//land1.tif")
+land2 = raster("Data//lab_3//land2.tif")
+land3 = raster("Data//lab_3//land3.tif")
+land4 = raster("Data//lab_3//land4.tif")
+land5 = raster("Data//lab_3//land5.tif")
+land6 = raster("Data//lab_3//land6.tif")
+land7 = raster("Data//lab_3//land7.tif")
+land8 = raster("Data//lab_3//land8.tif")
+land9 = raster("Data//lab_3//land9.tif")
+land10 = raster("Data//lab_3//land10.tif")
 #All landscape .tiffs (e.g., "land1.tiff") need to be in your working directory
 
 #Panel figure with all 10 landscapes
